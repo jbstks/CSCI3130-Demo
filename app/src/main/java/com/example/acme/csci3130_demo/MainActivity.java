b74fef00f4d08e5a50b4c8a5c72c920af90dd86f
Joanna <18332426+jbstks@users.noreply.github.com>
2018-05-15 00:55:39 -0300

    Multiple changes
    Added what was asked for in Part e of Question 3:
    
     - Add a button and a text field to the UI.
    - The user should input text on the field and click the button after.
    - The text from the field should then be copied to the textbox that was already in the layout.
    
    Also changed a few minor things:
    
    - Name of application (removed _ in CSCI3130_Demo)
    - Aligned textbox to the text field instead of the bottom of the application

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.acme.csci3130_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+/**
+ * @author Joanna Bistekos (B00710704)
+ *
+ * Followed tutorial at
+ * https://developer.android.com/training/basics/firstapp/starting-activity#java
+ * making modifications where necessary
+ */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Function to be called when the SEND button is pressed by the user
+     *
+     * @param view the view object that was clicked
+     */
+    public void sendMessage(View view) {
+        // Updates the value in textView2
+        EditText editText = findViewById(R.id.inputField);
+        String message = editText.getText().toString();
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
